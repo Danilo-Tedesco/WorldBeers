cec9bce2cb734e69ae5907be881a65570b52e722
Danilo Tedesco <tedesco.danilo94@gmail.com>
2022-05-10 13:50:02 +0200

    First initial commit

---
+++ WorldBeers/WorldBeers/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  WorldBeers
+//
+//  Created by Danilo Tedesco on 10/05/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
